36436214be9a0aedd868a86fa8c0382600874885
Vi1679 <baranov2008@ngs.ru>
2023-02-18 20:03:08 +0700

    Добавлены .gitignore и .clang-format

--- nomer1.c
+++ nomer1.c
@@ -3,22 +3,23 @@
 
 void init(int* arr, int n)
 {
-	arr = malloc(n * sizeof(int));
-	int i;
-	for(i = 0; i < n; ++i){
-		arr[i] = i;
-		printf("%d\n", arr[i]);
-	}
+    arr = malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; ++i) {
+        arr[i] = i;
+        printf("%d\n", arr[i]);
+    }
 }
 
-int main(){
-	int* arr = NULL;
-	int n = 10;
-	//arr = malloc(n * sizeof(int));
-	init(arr, n);
-	/*int i;
-	for(i = 0; i < n; ++i){
-		printf("%d\n", arr[i]);
-	}*/
-	return 0;
+int main()
+{
+    int* arr = NULL;
+    int n = 10;
+    // arr = malloc(n * sizeof(int));
+    init(arr, n);
+    /*int i;
+    for(i = 0; i < n; ++i){
+            printf("%d\n", arr[i]);
+    }*/
+    return 0;
 }

905a51352361ebedd0f1062f9f48ff9651b487a2
Vitalii <baranov2008@ngs.ru>
2023-04-25 18:26:31 +0700

    proga

--- command.c
+++ command.c
@@ -49,7 +49,7 @@ int encode(uint32_t code_point, CodeUnit* code_units)
         code_units->code[1] = 0x80 | ((code_point & 0xfc0) >> 6);
         code_units->code[2] = 0x80 | (code_point & 0x3f);
         code_units->length = 3;
-        return 0;   
+        return 0;
     } else if (count <= 21) {
         code_units->code[0] = 0xf0 | (code_point >> 18);
         code_units->code[1] = 0x80 | ((code_point & 0x3f000) >> 12);
@@ -64,7 +64,31 @@ int encode(uint32_t code_point, CodeUnit* code_units)
 uint32_t decode(const CodeUnit* code_unit)
 {
     uint32_t code_point = 0;
-    if((code_unit->code[0] >> 7) == 0){
-        
+    if ((code_unit->code[0] >> 7) == 0) {
+        code_point = code_unit->code[0];
+    } else if (code_unit->code[0] <= 0xdf) {
+        code_point = ((code_unit->code[0] & 0x1f) << 6)
+                | (code_unit->code[1] & 0x3f);
+    } else if (code_unit->code[0] <= 0xef) {
+        code_point = ((code_unit->code[0] & 0xf) << 12)
+                | ((code_unit->code[1] & 0x3f) << 6)
+                | (code_unit->code[2] & 0x3f);
+    } else if (code_unit->code[0] <= 0xf7) {
+        code_point = ((code_unit->code[0] & 0x7) << 18)
+                | ((code_unit->code[1] & 0x3f) << 12)
+                | ((code_unit->code[2] & 0x3f) << 6)
+                | (code_unit->code[3] & 0x3f);
     }
+    return code_point;
+}
+
+int read_next_code_unit(FILE* in, CodeUnit* code_units)
+{
+    
+}
+
+int write_code_unit(FILE* out, const CodeUnit* code_unit)
+{
+    fwrite(code_unit, sizeof(uint8_t), code_unit->length, out);
+    return 0;
 }

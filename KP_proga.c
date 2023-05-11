7ce2b0e338560b073c8335af4fbc9ea869f013ad
Vitalii <baranov2008@ngs.ru>
2023-05-11 21:45:23 +0700

    KYrsa4

---
+++ KP_proga.c
@@ -0,0 +1,77 @@
+#include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char* str = malloc(sizeof(char) * 100);
+    char* s = malloc(sizeof(char) * 100);
+    fgets(str, 100, stdin);
+    fflush(stdin);
+    struct stack* num = stack_create(100);
+    struct stack_char* ops = stack_create_char(100);
+    int k = 0;
+    for (int i = 0; i < strlen(str); ++i) {
+        if (str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4'
+            || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8'
+            || str[i] == '9' || str[i] == '0' || str[i] == '.') {
+            s[k++] = str[i];
+        } else if (str[i] != ' ') {
+            stack_push(num, atof(s));
+            memset(s, 0, k);
+            k = 0;
+        }
+        if (str[i] == '-' || str[i] == '+' || str[i] == '/' || str[i] == '*'
+            || str[i] == '(' || str[i] == ')') {
+            if (((ops->a[stack_size_char(ops) - 1] == '*'
+                  || ops->a[stack_size_char(ops) - 1] == '/')
+                 && (str[i] == '+' || str[i] == '-'))
+                || (ops->a[stack_size_char(ops) - 1] == '-' && str[i] == '+')) {
+                float y = stack_pop(num), x = stack_pop(num);
+                int op = stack_pop_char(ops);
+                if (op == 43) {
+                    x = x + y;
+                }
+                if (op == 42) {
+                    x = x * y;
+                }
+                if (op == 47) {
+                    x = x / y;
+                }
+                if (op == 45) {
+                    x = x - y;
+                }
+                stack_push(num, x);
+            }
+            stack_push_char(ops, str[i]);
+        }
+    }
+    for (int i = 0; i < stack_size_char(ops) + 1; ++i) {
+        float y = stack_pop(num), x = stack_pop(num);
+        // printf("x = %f y = %f ", x, y);
+        int op = stack_pop_char(ops);
+        if (op == 43) {
+            x = x + y;
+            // printf("+\n%f", x);
+        }
+        if (op == 42) {
+            x = x * y;
+            // printf("*\n%f", x);
+        }
+        if (op == 47) {
+            x = x / y;
+            // printf("/\n%f", x);
+        }
+        if (op == 45) {
+            x = x - y;
+            // printf("-\n%f", x);
+        }
+        stack_push(num, x);
+    }
+    printf("Ответ = %f\n", stack_pop(num));
+    stack_free(num);
+    stack_free_char(ops);
+    free(str);
+    free(s);
+}
---
+++ stack.c
@@ -0,0 +1,101 @@
+#include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack* stack_create(int maxsize)
+{
+    struct stack* s = malloc(sizeof(*s));
+    if (s != NULL) {
+        s->a = malloc(sizeof(float) * maxsize);
+        if (s->a == NULL) {
+            free(s);
+            return NULL;
+        }
+        s->size = 0;
+        s->top = 0;
+        s->maxsize = maxsize;
+    }
+    return s;
+}
+
+void stack_free(struct stack* s)
+{
+    free(s->a);
+    free(s);
+}
+
+int stack_size(struct stack* s)
+{
+    return s->size;
+}
+
+int stack_push(struct stack* s, float value)
+{
+    if (s->top < s->maxsize) {
+        s->a[s->top++] = value;
+        s->size++;
+    } else {
+        fprintf(stderr, "Stack overflow\n");
+        return -1;
+    }
+    return 0;
+}
+
+float stack_pop(struct stack* s)
+{
+    if (s->top == 0) {
+        fprintf(stderr, "Stack underflow\n");
+        return -1;
+    }
+    s->size--;
+    return s->a[--s->top];
+}
+
+struct stack_char* stack_create_char(int maxsize)
+{
+    struct stack_char* s = malloc(sizeof(*s));
+    if (s != NULL) {
+        s->a = malloc(sizeof(char) * maxsize);
+        if (s->a == NULL) {
+            free(s);
+            return NULL;
+        }
+        s->size = 0;
+        s->top = 0;
+        s->maxsize = maxsize;
+    }
+    return s;
+}
+
+void stack_free_char(struct stack_char* s)
+{
+    free(s->a);
+    free(s);
+}
+
+int stack_size_char(struct stack_char* s)
+{
+    return s->size;
+}
+
+int stack_push_char(struct stack_char* s, char value)
+{
+    if (s->top < s->maxsize) {
+        s->a[s->top++] = value;
+        s->size++;
+    } else {
+        fprintf(stderr, "Stack overflow\n");
+        return -1;
+    }
+    return 0;
+}
+
+int stack_pop_char(struct stack_char* s)
+{
+    if (s->top == 0) {
+        fprintf(stderr, "Stack underflow\n");
+        return -1;
+    }
+    s->size--;
+    return s->a[--s->top];
+}

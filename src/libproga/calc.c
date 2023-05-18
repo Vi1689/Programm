6b7d0d5b79928b51a8f7edb9f8e4e9fcea7ae588
Vitalii <baranov2008@ngs.ru>
2023-05-18 19:24:03 +0700

    Kyrsa4

---
+++ src/libproga/calc.c
@@ -0,0 +1,45 @@
+#include "calc.h"
+#include "stack.h"
+
+int calc(struct stack* number, struct stack* op)
+{
+    float a = stack_pop(number), b = stack_pop(number);
+    int c = stack_pop(op);
+    switch (c) {
+    case 43:
+        if (stack_push(number, b + a)) {
+            return 1;
+        }
+        break;
+    case 45:
+        if (stack_push(number, b - a)) {
+            return 1;
+        }
+        break;
+    case 42:
+        if (stack_push(number, b * a)) {
+            return 1;
+        }
+        break;
+    case 47:
+        if (!a) {
+            return 1;
+        }
+        if (stack_push(number, b / a)) {
+            return 1;
+        }
+        break;
+    }
+    return 0;
+}
+
+int rang(int a)
+{
+    if (a == 43 || a == 45) {
+        return 1;
+    }
+    if (a == 42 || a == 47) {
+        return 2;
+    }
+    return 0;
+}

5ba03eacece890d121634774e300d6f40fbe76cd
Vitalii <baranov2008@ngs.ru>
2023-09-27 10:37:55 +0700

    Laba_3

--- OOP/laba_3/laba_3.cpp
+++ OOP/laba_3/laba_3.cpp
@@ -6,7 +6,6 @@ using namespace std;
 class matrix {
 private:
     int** a;
-    int mn;
     int m;
     int n;
     int summ;
@@ -14,7 +13,6 @@ private:
 public:
     matrix()
     {
-        mn = 0;
         n = 0;
         m = 0;
         summ = 0;
@@ -23,150 +21,88 @@ public:
 
     matrix(int size)
     {
-        mn = size;
-        n = 0;
-        m = 0;
+        n = size;
+        m = size;
         summ = 0;
-        a = create_matrix();
+        int** b = new int*[m];
+        for (int i = 0; i < m; ++i) {
+            b[i] = new int[n];
+            for (int j = 0; j < n; ++j) {
+                b[i][j] = 0;
+                if (i == j) {
+                    b[i][j] = 1;
+                }
+            }
+        }
+        a = b;
     }
 
     matrix(int size1, int size2)
     {
         m = size1;
         n = size2;
-        mn = 0;
         summ = 0;
-        a = create_matrix();
-    }
-
-    int** create_matrix()
-    {
-        if (mn > 0) {
-            int** a = new int*[mn];
-            for (int i = 0; i < mn; ++i) {
-                a[i] = new int[mn];
-                for (int j = 0; j < mn; ++j) {
-                    a[i][j] = 0;
-                    if (i == j) {
-                        a[i][j] = 1;
-                    }
-                }
-            }
-            return a;
+        int** b = new int*[m];
+        for (int i = 0; i < m; ++i) {
+            b[i] = new int[n];
         }
-        if (m > 0) {
-            int** a = new int*[m];
-            for (int i = 0; i < m; ++i) {
-                a[i] = new int[n];
-            }
-            return a;
-        }
-        return NULL;
+        a = b;
     }
 
     void input()
     {
-        if (mn > 0) {
-            for (int i = 0; i < mn; ++i) {
-                for (int j = 0; j < mn; ++j) {
-                    cin >> a[i][j];
-                }
-            }
-        }
-        if (m > 0) {
-            for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    cin >> a[i][j];
-                }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cin >> a[i][j];
             }
         }
     }
 
     void autofill()
     {
-        if (mn > 0) {
-            for (int i = 0; i < mn; ++i) {
-                for (int j = 0; j < mn; ++j) {
-                    a[i][j] = rand() % 100;
-                }
-            }
-        }
-        if (m > 0) {
-            for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    a[i][j] = rand() % 100;
-                }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                a[i][j] = rand() % 100;
             }
         }
     }
 
     void ij()
     {
-        if (mn > 0) {
-            for (int i = 0; i < mn; ++i) {
-                for (int j = 0; j < mn; ++j) {
-                    a[i][j] = i * j;
-                }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                a[i][j] = i * j;
             }
         }
     }
 
     void print()
     {
-        if (mn > 0) {
-            for (int i = 0; i < mn; ++i) {
-                for (int j = 0; j < mn; ++j) {
-                    cout << a[i][j] << " ";
-                }
-                cout << "\n";
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << a[i][j] << " ";
             }
             cout << "\n";
         }
-        if (m > 0) {
-            for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    cout << a[i][j] << " ";
-                }
-                cout << "\n";
-            }
-            cout << "\n";
-        }
+        cout << "\n";
     }
 
     void summa()
     {
-        if (mn > 0) {
-            for (int i = 0; i < mn; ++i) {
-                for (int j = 0; j < mn; ++j) {
-                    summ += a[i][j];
-                }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                summ += a[i][j];
             }
-            cout << summ << "\n";
         }
-        if (m > 0) {
-            for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    summ += a[i][j];
-                }
-            }
-            cout << summ << "\n";
-        }
+        cout << summ << "\n";
     }
 
     ~matrix()
     {
-        if (mn > 0) {
-            for (int i = 0; i < mn; ++i) {
-                delete[] a[i];
-            }
-            delete[] a;
+        for (int i = 0; i < m; ++i) {
+            delete[] a[i];
         }
-        if (m > 0) {
-            for (int i = 0; i < m; ++i) {
-                delete[] a[i];
-            }
-            delete[] a;
-        }
+        delete[] a;
     }
 };
 

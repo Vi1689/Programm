94b18ef244744665f0602a075213a5f32c66d028
Vitalii <baranov2008@ngs.ru>
2023-11-07 20:24:25 +0700

    laba

---
+++ CAOD/laba_3/fibheap.c
@@ -0,0 +1,6 @@
+#include "fibheap.h"
+
+struct fibheap* fibheap_insert(struct fibheap* heap, int key, char* value)
+{
+    
+}
--- OOP/kursach/kursach.cpp
+++ OOP/kursach/kursach.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
+#include "oke.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "qwe"
-         << "\n";
+    srand(time(NULL));
+    char a[10][10];
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            a[i][j] = '0';
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }

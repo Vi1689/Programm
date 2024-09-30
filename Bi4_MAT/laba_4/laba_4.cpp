cb9297a76bf2bb9f165210b157be05f78f8c09d8
Vitalii <baranov2008@ngs.ru>
2024-10-01 01:46:08 +0700

    Починил лаюу с вичислительной матики

--- Bi4_MAT/laba_4/laba_4.cpp
+++ Bi4_MAT/laba_4/laba_4.cpp
@@ -6,12 +6,12 @@ int main()
     std::vector<int> a;
     std::vector<int> b;
     std::vector<int> c(10);
-    for(int i = 1; i <= 10; ++i){
+    for (int i = 1; i <= 10; ++i) {
         a.push_back(i);
         b.push_back(i * -1);
+        c.push_back(a[i - 1] + b[i - 1]);
     }
-    c = a + b;
-    for(int i = 0; i < 10; ++i){
+    for (int i = 0; i < 10; ++i) {
         std::cout << c[i] << " ";
     }
 }
